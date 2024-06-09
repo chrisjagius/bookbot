149b617494c94635a0890ec346dba73e53dad406
chrisjagius <chrisagius@fastmail.com>
2024-06-08 23:49:55 -0400

    feat: complete text report functionality

--- main.py
+++ main.py
@@ -6,21 +6,39 @@ def get_word_count(text: str) -> int:
     return len(words)
 
 
-def get_char_counts(text: str) -> dict[str, int]:
+def get_sorted_alpha_counts(text: str) -> list[tuple[str, int]]:
     char_dict = {}
     for char in text.lower():
-        if char not in char_dict:
-            char_dict[char] = 1
-        else:
+        if char in char_dict:
             char_dict[char] += 1
-    return char_dict
+        else:
+            char_dict[char] = 1
+    sorted = []
+    for char, count in char_dict.items():
+        if char.isalpha():
+            sorted.append((char, count))
+    sorted.sort(reverse=True, key=lambda char_count: char_count[1])
+    return sorted
+
+
+def get_char_count_lines(char_counts: list[tuple[str, int]]) -> str:
+    char_count_lines = ""
+    for count in char_counts:
+        char_count_lines += f"The '{count[0]}' character was found {count[1]} times\n"
+    return char_count_lines
+
+
+def get_report(path: str, word_count: int, char_counts: list[tuple[str, int]]) -> str:
+    return f"--- Begin report of {path} ---\n{word_count} words found in the document\n\n{get_char_count_lines(char_counts)}--- End report ---"
 
 
 def main() -> int:
-    with open("books/frankenstein.txt") as f:
+    text_path = "books/frankenstein.txt"
+    with open(text_path) as f:
         file_contents = f.read()
-    print(get_word_count(file_contents))
-    print(get_char_counts(file_contents))
+    word_count = get_word_count(file_contents)
+    alpha_counts = get_sorted_alpha_counts(file_contents)
+    print(get_report(text_path, word_count, alpha_counts))
 
 
 if __name__ == "__main__":
